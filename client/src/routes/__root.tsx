5c3b65f0834f25fc5d040563919d1e2bd78a7820
Shrinivas2708 <ssherikar2005@gmail.com>
2025-09-28 20:08:23 +0530

    started client dev using tanstack router

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,16 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
