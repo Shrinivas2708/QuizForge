fc7c08dc5faca5925f1fe26d61c5faa20b65b195
Shrinivas2708 <ssherikar2005@gmail.com>
2025-10-14 14:56:59 +0530

    dk

--- room/vite.config.ts
+++ room/vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [
-    tanstackRouter({ autoCodeSplitting: true }),
-    viteReact(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tailwindcss(),
+    tanstackStart(),
+    viteReact(),
   ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
 })
+
+export default config
