0217268525141dcf0b9a08a25041a64f214ed68e
Shrinivas2708 <ssherikar2005@gmail.com>
2025-10-15 22:20:08 +0530

    prod working

--- client/src/lib/exports.ts
+++ client/src/lib/exports.ts
@@ -1,6 +1,6 @@
 export const SERVER_URL: string =
   import.meta.env.VITE_ENV === 'dev'
-    ? 'http://127.0.0.1:8787'
+    ? 'http://localhost:8787'
     : 'https://server.ssherikar2005.workers.dev' // Changed this!
 
 export const ROOMS_URL: string = 
